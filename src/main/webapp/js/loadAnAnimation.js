9b79c05b3d785071f02826c450e8dc381ddfb889
joliveros <oliverosjose@gmail.com>
2016-12-29 10:54:57 -0400

    loadAnAnimation listo

---
+++ src/main/webapp/js/loadAnAnimation.js
@@ -0,0 +1,14 @@
+/**
+ * Created by joliveros on 29/12/2016.
+ */
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'image', {preload: preload, create: create});
+
+function preload() {
+    game.load.atlasJSONHash('bot', 'assets/loadAnAnimation/running_bot.png', 'assets/loadAnAnimation/running_bot.json')
+}
+
+function create() {
+    var bot = game.add.sprite(200, 200, 'bot');
+    bot.animations.add('correr');
+    bot.animations.play('correr', 15, true);
+}
